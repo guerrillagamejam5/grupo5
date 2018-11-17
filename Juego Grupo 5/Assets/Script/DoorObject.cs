e42a9c6f3b88ffcce7acb911fe86d2839f106ab4
DeLoW92 <josecarlosmontano92@gmail.com>
2018-11-17 07:13:59 +0100

    Toggle door system added. (Needed button GUI)

---
+++ Juego Grupo 5/Assets/Script/DoorObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorObject : MonoBehaviour {
+
+    private bool open = false;
+
+    // Default constructor
+    public DoorObject ()
+    {
+        open = false;
+    }
+
+    public DoorObject (bool state)
+    {
+        open = state;
+    }
+
+    public void setState(bool state)
+    {
+        open = state;
+    }
+
+    public bool getState()
+    {
+        return open;
+    }
+}
---
+++ Juego Grupo 5/Assets/Script/ToggleDoors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDoors : MonoBehaviour {
+
+	// Event for toggle doors
+	public void ToogleDoors () {
+        GameObject[] doors = GameObject.FindGameObjectsWithTag("Door");
+        foreach (GameObject door in doors) {
+            door.GetComponent<DoorObject>().setState(!door.GetComponent<DoorObject>().getState());
+            Debug.Log(door.GetComponent<DoorObject>().getState());
+        }
+	}
+}

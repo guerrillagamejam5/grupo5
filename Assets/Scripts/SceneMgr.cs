60e0aa7c13eb8f299bfd1085b5ca9a2b3e96a66a
guerrillagamejam5 <45105403+guerrillagamejam5@users.noreply.github.com>
2018-11-17 16:20:41 +0100

    Add files via upload

---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : MonoBehaviour {
+    public string[] scenas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void CambiarScena(int s)
+    {
+        StartCoroutine(CambioScena(scenas[0]));
+    }
+
+    IEnumerator CambioScena(string scene)
+    {
+        yield return new WaitForSeconds(0.3f);
+        SceneManager.LoadScene(scene);
+    }
+}

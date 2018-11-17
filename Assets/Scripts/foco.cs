bb66fb73105bb2dd1394398eb2dd97cab12337e3
guerrillagamejam5 <45105403+guerrillagamejam5@users.noreply.github.com>
2018-11-17 05:40:09 +0100

    Add files via upload

---
+++ Assets/Scripts/foco.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foco : MonoBehaviour {
+
+    public float alphaLevel;
+
+
+	// Use this for initialization
+	void Start () {
+        alphaLevel = 0.30f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          
+        transform.GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, alphaLevel);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        Debug.Log("Has perdido");
+    }
+
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public bool encendido;
+    public GameObject[] focos;
+    public int velocidad;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        velocidad = 5;
+        encendido = true;
+        focos = GameObject.FindGameObjectsWithTag("Foco");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //si activo luces
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            cambioOnOff();
+        }
+
+        movimientoJugador();
+
+        comprueboFocos();
+
+
+
+    }
+
+    void cambioOnOff()
+    {
+        if (encendido)
+        {
+            encendido = false;
+            Debug.Log("Luces apagadas");
+        }
+        else
+        {
+            encendido = true;
+            Debug.Log("Luces encendidas");
+
+        }
+    }
+
+
+    void comprueboFocos()
+    {
+        foreach (var foco in focos)
+        {
+
+
+            if (encendido)
+            {
+                foco.GetComponent<BoxCollider2D>().enabled = true;
+                foco.SetActive(true);
+                foco.transform.GetChild(0).gameObject.SetActive(true);
+            }
+
+            else
+            {
+                foco.GetComponent<BoxCollider2D>().enabled = false;
+                foco.SetActive(false);
+                foco.transform.GetChild(0).gameObject.SetActive(false);
+
+            }
+        }
+    }
+
+    void movimientoJugador()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidad, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
